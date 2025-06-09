e06e2590c1ee10e887eeaae659e6c1fa78bd9f06
MatheusMagno <matheusmagno7898@gmail.com>
2025-06-09 16:46:36 -0300

    teste

--- server.js
+++ server.js
@@ -26,6 +26,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASS,
   port: process.env.DB_PORT,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 // Teste de conexão
